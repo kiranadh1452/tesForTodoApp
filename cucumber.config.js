278d3f614286d1181dab8cf4466ea73952f15cec
Kiran Adhikari <kiranadh1452@gmail.com>
2022-04-25 09:19:05 +0545

    step definition functions created, hooks defined

--- cucumber.config.js
+++ cucumber.config.js
@@ -1,37 +1,28 @@
-// const {Before,After,BeforeAll,AfterAll, setDefaultTimeout} = require('@cucumber/cucumber');
-// const {chromium} = require('playwright');
-// const {expect} = require('@playwright/test');
-// const {clearDropbox} = require('./tests/acceptance/databaseHelper/deleteInstances.js');
+const {Before, BeforeAll, After, AfeterAll, setDefaultTimeout, AfterAll} = require('@cucumber/cucumber');
+const {chromium} = require('playwright');
+const {expect} = require('@playwright/test');
 
-// global.expect = expect;
-// setDefaultTimeout(1000*1000);
+global.expect = expect;
+setDefaultTimeout(1000*1000);
 
-// BeforeAll(async function(){
-//   global.browser = await chromium.launch({
-//     slowMo: 500,
-//     headless: false,
-//     channel: "chrome"
-//   });
-// });
+BeforeAll(async function(){
+  global.browser = await chromium.launch({
+    slowMo: 500,
+    headless: false,
+    channel: "chrome"
+  });
+});
 
-// AfterAll(async function(){
-//   await global.browser.close();
-//   clearDropbox();
-// });
+AfterAll(async function(){
+  await global.browser.close();
+});
 
-// Before(async function(){
-//   global.context = await global.browser.newContext();
-//   global.page = await global.context.newPage();
-// });
+Before(async function(){
+  global.context = await global.browser.newContext();
+  global.page = await global.context.newPage();
+});
 
-// After(async function(){
-//   // await global.page.goto('http://localhost/administrator/index.php?option=com_dropbox&view=dropboxes');
-//   // const selectAllBtn = await global.page.locator("//input[@title='Check All Items']");
-//   // await selectAllBtn.click();
-
-//   // const deleteAllBtn = await global.page.locator("//div[@id='toolbar-delete']/button");
-//   // await deleteAllBtn.click();
-  
-//   await global.page.close();
-//   await global.context.close();
-// });
+After(async function(){
+  await global.page.close();
+  await global.context.close();
+})
---
+++ tests/acceptance/stepDefinitions/addTodoContext.js
@@ -0,0 +1,10 @@
+const {Given, When, Then, And, But } = require('@cucumber/cucumber');
+
+Given('the user has browsed to home page', function () {
+});
+
+When('the user enters {string} in todo section', function (string) {
+});
+
+Then('the user should see {string} in todo list', function (string) {
+});
